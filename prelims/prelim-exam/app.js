4ae72a83e757a581291d050cc56af6e1500c2110
JRQ12345 <122766204+JRQ12345@users.noreply.github.com>
2023-02-08 15:51:12 +0800

    1 file modified, 3 added

---
+++ prelims/prelim-exam/app.js
@@ -0,0 +1,5 @@
+var greetings = require('./function.js');
+
+console.log(greetings('Jose Raphael Quiambao'));
+console.log(congratulations('Jose Raphael Quiambao'));
+console.log(farewell('Jose Raphael Quiambao'));
---
+++ prelims/prelim-exam/function.js
@@ -0,0 +1,13 @@
+function greet(name) {
+  return 'hello ' + name;
+}
+
+function congratulation(name) {
+  return 'hello ' + name;
+}
+
+function farewell(name) {
+  return 'hello ' + name;
+}
+
+module.exports = { greet, congratulation, farewell };
